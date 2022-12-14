71dbd95fc2f31a2130633c04a09486bec50364d2
Rick <ricardohs@ricardohs.com>
2022-12-14 11:10:57 -0600

    Responsive desing for discover page

--- lib/discover_page.dart
+++ lib/discover_page.dart
@@ -5,80 +5,118 @@ import 'package:lets_net_work/model/event_data.dart';
 class DiscoverPage extends StatelessWidget {
   const DiscoverPage({Key? key}) : super(key: key);
 
-  Widget tile(EventData event) {
-    List<Widget> chips = [];
-    for(int x=0;x<event.targetAreas.length;x++){
-      chips.add(Chip(label: Text(event.targetAreas[x])));
-    }
-    return ListTile(
-      leading: ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 26),
-          color: Color.fromARGB(70, 0, 179, 255),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Date:"),
-              Text(
-                "${event.month}/${event.day}/${event.year}",
-                style: TextStyle(fontSize: 16),
-              ),
-            ],
-          ),
-        ),
-      ),
-      title: Text(event.title),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(event.description),
-          Text("Topic: ${event.topic}"),
-          Text("Location: ______________"),
-        ],
-      ),
-      trailing: Container(
-        width: 350,
-        child: Wrap(
-          children: chips,
-        ),
-      ),
-      isThreeLine: true,
-    );
-  }
-
-  List<Widget> eventsToTiles(List<EventData> events) {
-    List<Widget> tiles = [];
-    for (int x = 0; x < events.length; x++) {
-      var event = events[x];
-      tiles.add(tile(event));
-    }
-    return tiles;
-  }
-
-  Future<Widget> eventsColumn() async {
-    final queryShot =
-        await FirebaseFirestore.instance.collection('events').get();
-
-    List<EventData> events =
-        queryShot.docs.map((doc) => EventData.fromJson(doc.data())).toList();
-
-    List<Widget> tiles = [];
-    tiles.add(const Text(
-      "UPCOMING EVENTS",
-      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
-    ));
-    tiles.add(SizedBox(height: 16));
-
-    tiles = tiles + eventsToTiles(events);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: tiles,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+    Widget tile(EventData event) {
+      List<Widget> chips = [];
+      for (int x = 0; x < event.targetAreas.length; x++) {
+        chips.add(Chip(label: Text(event.targetAreas[x])));
+      }
+
+      final currentWidth = MediaQuery.of(context).size.width;
+      if (currentWidth > 600) {
+        return ListTile(
+          leading: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 26),
+              color: Color.fromARGB(70, 0, 179, 255),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Date:"),
+                  Text(
+                    "${event.month}/${event.day}/${event.year}",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          title: Text(event.title),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(event.description),
+              Text("Topic: ${event.topic}"),
+              Text("Location: ______________"),
+            ],
+          ),
+          trailing: Container(
+            width: MediaQuery.of(context).size.width / 4,
+            child: Wrap(
+              children: chips,
+            ),
+          ),
+          isThreeLine: true,
+        );
+      }
+
+      return ListTile(
+        leading: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            color: Color.fromARGB(70, 0, 179, 255),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Date:"),
+                Text(
+                  "${event.month}/${event.day}/${event.year}",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ),
+        title: Text(event.title),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(event.description),
+            Text("Topic: ${event.topic}"),
+            Text("Location: ______________"),
+            SizedBox(height: 10),
+            Wrap(children: chips),
+          ],
+        ),
+        isThreeLine: true,
+      );
+    }
+
+    List<Widget> eventsToTiles(List<EventData> events) {
+      List<Widget> tiles = [];
+      for (int x = 0; x < events.length; x++) {
+        var event = events[x];
+        tiles.add(tile(event));
+      }
+      return tiles;
+    }
+
+    Future<Widget> eventsColumn() async {
+      final queryShot =
+          await FirebaseFirestore.instance.collection('events').get();
+
+      List<EventData> events =
+          queryShot.docs.map((doc) => EventData.fromJson(doc.data())).toList();
+
+      List<Widget> tiles = [];
+      tiles.add(const Text(
+        "UPCOMING EVENTS",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+      ));
+      tiles.add(SizedBox(height: 16));
+
+      tiles = tiles + eventsToTiles(events);
+      return SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: tiles,
+        ),
+      );
+    }
+
     return Container(
       padding: EdgeInsets.all(20),
       child: FutureBuilder(
